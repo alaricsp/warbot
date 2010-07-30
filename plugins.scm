5358630b04ec1b1ba54cef22bd81ac85b93a37bf
Alaric Snell-Pym <alaric@snell-pym.org.uk>
2010-07-30 02:44:58 +0100

    Initial release of WarBot

---
+++ plugins.scm
@@ -0,0 +1,100 @@
+
+(register-plugin! 'log
+		  (lambda (name . conf)
+		    (printf "plugin log: (configure ~S)\n" conf)
+		    (make-plugin
+		     name
+		     (lambda ()
+		       (printf "plugin log: (enable)\n")
+		       (void))
+		     (lambda ()
+		       (printf "plugin log: (disable)\n")
+		       (void))
+		     (lambda (nick channel)
+		       (printf "plugin log: (join ~S ~S)\n" nick channel)
+		       (void))
+		     (lambda (nick channel)
+		       (printf "plugin log: (leave ~S ~S)\n" nick channel)
+		       (void))
+		     (lambda (nick channel text)
+		       (printf "plugin log: (speak ~S ~S ~S)\n" nick channel text)
+		       (void))
+		     '()
+		     '())))
+
+(register-plugin! 'scheme
+		  (lambda (name)
+		    (make-plugin
+		     name
+		     (lambda ()
+		       (void))
+		     (lambda ()
+		       (void))
+		     (lambda (nick channel)
+		       (void))
+		     (lambda (nick channel)
+		       (void))
+		     (lambda (nick channel text)
+		       (void))
+		     (list
+		      (make-plugin-command
+		       (regexp "eval +(.*)")
+		       'eval
+		       "eval <scheme>: Run some arbitrary code"
+		       (lambda (nick reply-to channel all message)
+			 (let* ((sexpr (with-input-from-string message read))
+				(result
+				 (call-with-current-continuation
+				  (lambda (k)
+				    (with-exception-handler
+				     (lambda (exn)
+				       (k (sprintf "ERROR: ~S" exn)))
+				     (lambda ()
+				       (with-output-to-string (lambda () (write (eval sexpr))))))))))
+			   (irc:say *con* result reply-to)))))
+		     '())))
+
+(register-plugin! 'clock
+		  (lambda (name)
+		    (make-plugin
+		     name
+		     (lambda () (void))
+		     (lambda () (void))
+		     (lambda (nick channel) (void))
+		     (lambda (nick channel) (void))
+		     (lambda (nick channel text) (void))
+		     (list
+		      (make-plugin-command
+		       (regexp "time")
+		       #f
+		       "time: Tell the time"
+		       (lambda (nick reply-to channel all)
+			 (irc:say *con* (sprintf "It's ~A, ~A" (seconds->string (current-seconds)) nick) reply-to))))
+		     '())))
+
+(register-plugin! 'talking
+		  (lambda (name)
+		    (make-plugin
+		     name
+		     (lambda () (void))
+		     (lambda () (void))
+		     (lambda (nick channel) (void))
+		     (lambda (nick channel) (void))
+		     (lambda (nick channel text) (void))
+		     '()
+		     (list
+		      (make-plugin-command
+		       (regexp "wall +(.*)")
+		       'wall
+		       "wall <message>: Send a message to all channels"
+		       (lambda (nick reply-to channel all message)
+			 (for-each
+			  (lambda (channel)
+			    (irc:say *con* message channel))
+			  (map car *channels*))))
+		      (make-plugin-command
+		       (regexp "say +([^ ]+) +(.*)")
+		       'say
+		       "say <channel|user> <message>: Send a message"
+		       (lambda (nick reply-to channel all receiver message)
+			 (irc:say *con* message receiver)))))))
---
+++ warbot.scm
@@ -0,0 +1,546 @@
+(use irc)
+(use regex-case)
+(use matchable)
+
+(use posix)
+(use srfi-1)
+(use srfi-13)
+
+(define-record user
+  name
+  password
+  powers ;; list of symbols, or * for all
+  op-channels) ;; list of channels to op on, or * for all
+
+(define-record nick
+  name
+  authenticated-user ;; #f if not authenticated
+  channels ;; list of channels present on
+  op-channels) ;; list of channels with ops on
+
+(define-record channel
+  name
+  nicks ;; list of nicks in the channel
+  ops ;; list of op nicks in the channel
+  )
+
+(define-record plugin-command
+  regexp ; RE to match commands
+  power-needed ; #t for public command, or a symbol
+  help ; help text
+  handle!) ; lambda (nick send-replies-to channel . re-matches)
+
+(define-record plugin
+  name
+  enable! ; plugin enabled (configuration...)
+  disable! ; plugin disabled ()
+  join! ; nick has joined channel (nick channel)
+  leave! ; nick has left channel (nick channel)
+  speak! ; nick has spoken text in channel (nick channel text)
+  channel-commands ; list of plugin-commands
+  privmsg-commands) ; list of plugin-commands
+
+;; Config
+
+(define *botnick* "WarBot2")
+(define *conf-filename* "warbot.conf")
+
+;; Global state
+
+(define *plugin-factories* '())
+
+(define *plugins* '()) ;; Enabled plugins only
+
+(define *bot-user* (make-user *botnick* #f '* '*))
+
+(define *users* (list
+		 (cons *botnick* *bot-user*)))
+
+(define *nicks* '())
+
+(define *channels* '())
+
+(define *con*
+  (irc:connection server: "irc.warhead.org.uk" nick: "WarBot2") )
+
+;; Utility accessors
+
+(define (get-nick name)
+  (let ((search (assoc name *nicks*)))
+    (if search
+	(cdr search)
+	(let ((new-nick (make-nick name #f '() '())))
+	  (if (string=? name *botnick*)
+	      (nick-authenticated-user-set! new-nick *bot-user*))
+	  (set! *nicks* (cons
+		       (cons name new-nick)
+		       *nicks*))
+	  new-nick))))
+
+(define (has-power? nick power)
+  (let* ((nick* (get-nick nick))
+	 (user (nick-authenticated-user nick*)))
+    (if user
+	(or
+	 (eq? (user-powers user) '*)
+	 (member power (user-powers user)))
+	#f)))
+
+(define (is-authenticated? nick)
+  (let* ((nick* (get-nick nick))
+	 (user (nick-authenticated-user nick*)))
+    user))
+
+(define (get-channel name)
+  (let ((search (assoc name *channels*)))
+    (if search
+	(cdr search)
+	(let ((new-channel (make-channel name '() '())))
+	  (set! *channels* (cons
+		       (cons name new-channel)
+		       *channels*))
+	  new-channel))))
+
+(define (get-user name)
+  (let ((search (assoc name *users*)))
+    (if search
+	(cdr search)
+	#f)))
+
+(define (get-authed-user name password)
+  (let ((search (assoc name *users*)))
+    (if search
+	(if (string=? (user-password (cdr search)) password)
+	    (cdr search)
+	    #f)
+	#f)))
+
+(define (for-each-plugin func)
+  (for-each func *plugins*))
+
+(define (privmsg-commands)
+  (flatten (map privmsg-commands *plugins*)))
+
+(define (channel-commands)
+  (flatten (map privmsg-commands *plugins*)))
+
+(define (for-each-privmsg-command nick func)
+  (for-each (lambda (plugin)
+	      (for-each (lambda (cmd)
+			  (if
+			   (or (eq?  (plugin-command-power-needed cmd) #f) (has-power? nick (plugin-command-power-needed cmd)))
+			   (func cmd)))
+			(plugin-privmsg-commands plugin)))
+	    *plugins*))
+
+(define (for-each-channel-command nick func)
+  (for-each (lambda (plugin)
+	      (for-each (lambda (cmd)
+			  (if
+			   (or (eq? (plugin-command-power-needed cmd) #f) (has-power? nick (plugin-command-power-needed cmd)))
+			   (func cmd)))
+			(plugin-channel-commands plugin)))
+	    *plugins*))
+
+(define (register-plugin! plugin-name plugin-factory)
+  (set! *plugin-factories*
+	(cons (cons plugin-name plugin-factory) *plugin-factories*)))
+
+;; FIXME: Abstract nick-is-in-channel and nick-is-not-in-channel better!
+
+; op? is #t for an op, #f for a non-op, and anything else
+; if we don't actually know
+(define (nick-is-in-channel! channel nick op?)
+  (let ((nick* (get-nick nick))
+	(channel* (get-channel channel)))
+    (if (not (member nick* (channel-nicks channel*)))
+	(channel-nicks-set! channel* (cons nick* (channel-nicks channel*))))
+    (if (not (member channel* (nick-channels nick*)))
+	(nick-channels-set! nick* (cons channel* (nick-channels nick*))))
+
+    (case op?
+      ((#t) (begin
+	      (if (not (member nick* (channel-ops channel*)))
+		  (channel-ops-set! channel* (cons nick* (channel-ops channel*))))
+	      (if (not (member channel* (nick-op-channels nick*)))
+		  (nick-op-channels-set! nick* (cons channel* (nick-op-channels nick*))))))
+      ((#f) (begin
+	      (if (member nick* (channel-ops channel*))
+		  (channel-ops-set! channel* (filter (lambda (nick) (not (eq? nick nick*))) (channel-ops channel*))))
+	      (if (member channel* (nick-op-channels nick*))
+		  (nick-op-channels-set! nick* (filter (lambda (channel) (not (eq? channel channel*))) (nick-op-channels nick*)))))))
+    (void)))
+
+(define (nick-is-not-in-channel! channel nick)
+  (let ((nick* (get-nick nick))
+	(channel* (get-channel channel)))
+
+    (if (member nick* (channel-nicks channel*))
+	(channel-nicks-set! channel* (filter (lambda (nick) (not (eq? nick nick*))) (channel-nicks channel*))))
+    (if (member channel* (nick-channels nick*))
+	(nick-channels-set! nick* (filter (lambda (channel) (not (eq? channel channel*))) (nick-channels nick*))))
+
+    (if (member nick* (channel-ops channel*))
+	(channel-ops-set! channel* (filter (lambda (nick) (not (eq? nick nick*))) (channel-ops channel*))))
+    (if (member channel* (nick-op-channels nick*))
+	(nick-op-channels-set! nick* (filter (lambda (channel) (not (eq? channel channel*))) (nick-op-channels nick*))))
+    (void)))
+
+(define (is-op? channel nick)
+  (let* ((channel* (get-channel channel))
+	 (nick* (get-nick nick))
+	 (ops (channel-ops channel*)))
+    (member nick* ops)))
+
+(define (ensure-channel-membership! channel)
+  (if (assoc channel *channels*)
+      #t ; We're already there
+      (begin
+	(irc:join *con* channel)
+	(irc:action *con* "is ready for action" channel)
+	(set! *channels*
+	      (cons
+	       (cons channel (make-channel channel '() '()))
+	       *channels*)))))
+
+(define (load-user! name password powers op-channels)
+  (let ((existing-user (get-user name)))
+    (if existing-user
+	(begin
+	  (user-password-set! existing-user password)
+	  (user-powers-set! existing-user powers)
+	  (user-op-channels-set! existing-user op-channels))
+	(begin
+	  (let ((user (make-user name password powers op-channels)))
+	    (set! *users* (cons
+			   (cons name user)
+			   *users*)))))))
+
+(define (load-module! plugin)
+  (call-with-current-continuation
+   (lambda (k)
+     (with-exception-handler
+      (lambda (exn)
+	(printf "Error loading module ~A: ~S\n" plugin exn)
+	(k (void)))
+      (lambda ()
+	(load plugin))))))
+
+(define (get-plugin-factory plugin factories)
+  (cond
+   ((null? factories) #f)
+   ((eq? plugin (caar factories))
+    (cdar factories))
+   (else (get-plugin-factory plugin (cdr factories)))))
+
+(define (enable-plugin! plugin config)
+  (let ((plugin-factory (get-plugin-factory plugin *plugin-factories*)))
+    (if plugin-factory
+	(let ((plugin* (apply plugin-factory config)))
+	  ((plugin-enable! plugin*))
+	  (set! *plugins*
+		(cons plugin* *plugins*)))
+	(printf "Could not enable unknown plugin ~A\n" plugin))))
+
+(define (disable-plugins!)
+  (for-each
+   (lambda (plugin)
+     ((plugin-disable! plugin)))
+   *plugins*)
+  (set! *plugins* '()))
+
+(define (load-config!* configuration)
+  (if (null? configuration)
+      (void)
+      (begin
+	(match (car configuration)
+	       (('load module) (load-module! module))
+	       (('plugin plugin . config) (enable-plugin! plugin config))
+	       (('channel chan) (ensure-channel-membership! chan))
+	       (('user user pass powers op-channels) (load-user! user pass powers op-channels))
+	       (else (printf "ERROR: Unknown configuration directive: ~S\n" (car configuration))))
+	(load-config!* (cdr configuration)))))
+
+(define (load-config! configuration)
+  (disable-plugins!)
+  (load-config!* configuration))
+
+;; High-level event handlers
+
+(define (process-status-dump recipient)
+  (for-each (lambda (user-pair)
+	      (let ((user (cdr user-pair)))
+		(irc:say *con*
+			 (sprintf "user ~s powers ~s ops ~s" (user-name user) (user-powers user) (user-op-channels user))
+			 recipient)))
+	    *users*)
+  (for-each (lambda (nick-pair)
+	      (let ((nick (cdr nick-pair)))
+		(irc:say *con*
+			 (sprintf "nick ~s (auth: ~s) is in ~s and op in ~s" (nick-name nick) (if (nick-authenticated-user nick) (user-name (nick-authenticated-user nick)) #f) (map channel-name (nick-channels nick)) (map channel-name (nick-op-channels nick)))
+			 recipient)))
+	    *nicks*)
+  (for-each (lambda (channel-pair)
+	      (let ((channel (cdr channel-pair)))
+		(irc:say *con*
+			 (sprintf "chan ~s contains ~s and ~s ops" (channel-name channel) (map nick-name (channel-nicks channel)) (map nick-name (channel-ops channel)))
+			 recipient)))
+	    *channels*))
+
+(define (check-ops nick noisy?)
+  (and-let* ((nick* (get-nick nick))
+	     (user* (nick-authenticated-user nick*))
+	     (channels (user-op-channels user*))
+	     (op-channels (if (eq? '* channels)
+			      (map channel-name (nick-channels nick*))
+			      channels)))
+	    (for-each (lambda (channel)
+			(if (is-op? channel *botnick*)
+			    (if (is-op? channel nick)
+				(if noisy? (irc:action *con* (sprintf "need not give ops in ~A as you already have them" channel) nick))
+				(begin
+				  (if noisy? (irc:action *con* (sprintf "gives ops in ~A" channel) nick))
+				  (irc:command *con* (sprintf "MODE ~A +o ~A" channel nick))
+				  (nick-is-in-channel! channel nick #t)))
+			    (if noisy? (irc:action *con* (sprintf "cannot give ops in ~A as I don't have them myself!" channel) nick))))
+		      op-channels)))
+
+(define (process-attempt-auth nick user pass)
+  (let* ((user* (get-authed-user user pass))
+	 (nick* (get-nick nick)))
+    (if user*
+	(begin
+	  (nick-authenticated-user-set! nick* user*)
+	  (irc:say *con* (sprintf "Welcome, ~A" user) nick)
+	  (check-ops nick #t))
+	(begin
+	  (irc:say *con* "I'm sorry, that username/password is invalid" nick)))))
+
+(define (process-reload nick)
+  (load-config! (with-input-from-file *conf-filename* read-file))
+  (irc:say *con* "Reloaded configuration" nick))
+
+(define (send-help nick)
+  (let ((suggest-command (lambda (text)
+			   (irc:say *con* (sprintf " ~A" text) nick))))
+
+    (irc:say *con* "Here are the commands you can PM me:" nick)
+    (irc:say *con* " auth <user> <pass>: Authenticate yourself to me" nick)
+    (if (is-authenticated? nick)
+	(irc:say *con* " op me: Ask me to op you in channels you're authorised for" nick))
+
+    (if (has-power? nick 'dump)
+	(suggest-command "dump: Send a status dump"))
+    (if (has-power? nick 'reload)
+	(suggest-command "reload: Reload configuration file"))
+
+    (for-each-privmsg-command nick
+     (lambda (command)
+       (suggest-command
+	(plugin-command-help command))))
+
+    (for-each-channel-command nick
+     (lambda (command)
+       (suggest-command
+	(plugin-command-help command))))
+
+    (irc:say *con* "That's all." nick)))
+
+(define (privmsg nick body)
+  (regex-case
+   body
+   ("dump" _
+    (if (has-power? nick 'dump)
+	(process-status-dump nick)
+	(send-help nick)))
+   ("auth +([^ ]*) +([^ ]*)" (all user pass)
+    (process-attempt-auth nick user pass))
+   ("reload" _
+    (if (has-power? nick 'reload)
+	(process-reload nick)
+	(send-help nick)))
+   ("op me" _
+    (check-ops nick #t))
+   (else
+    (let ((matches 0))
+      (for-each-privmsg-command nick
+       (lambda (command)
+	 (let ((match (string-match (plugin-command-regexp command) body)))
+	   (if match
+	       (begin
+		 (apply (plugin-command-handle! command) (append (list nick nick #f) match))
+		 (set! matches (+ matches 1)))))))
+
+      (for-each-channel-command nick
+       (lambda (command)
+	 (let ((match (string-match (plugin-command-regexp command) body)))
+	   (if match
+	       (begin
+		 (apply (plugin-command-handle! command) (append (list nick nick #f) match))
+		 (set! matches (+ matches 1)))))))
+
+      (if (= matches 0)
+	  (send-help nick))))))
+
+(define *mention-re*
+  (regexp `(seq bos ,*botnick* (? ":") (* whitespace) (submatch (* any)) eos) #t))
+
+(define (send-channel-help nick)
+  (let ((suggest-command (lambda (help)
+			   (irc:say *con* (sprintf " ~A: ~A" *botnick* help) nick))))
+
+    (irc:say *con* "Here are the commands you can use in channels I'm listening to:" nick)
+    (for-each-channel-command nick
+     (lambda (command)
+       (suggest-command
+	(plugin-command-help command))))
+    (irc:say *con* "That's all." nick)))
+
+(define (channel-mention nick channel body)
+  (nick-is-in-channel! channel nick 'unknown)
+  (let* ((match (string-match *mention-re* body))
+	 (content (cadr match)))
+    (let ((matches 0))
+      (for-each-channel-command nick
+       (lambda (command)
+	 (let ((match (string-match (plugin-command-regexp command) content)))
+	   (if match
+	       (begin
+		 (apply (plugin-command-handle! command) (append (list nick channel channel) match))
+		 (set! matches (+ matches 1)))))))
+
+      (if (= matches 0)
+	  (send-channel-help nick)))))
+
+;; Low-level handlers
+
+(define (join message)
+  (let ((channel (irc:message-receiver message))
+	(nick (irc:message-sender message)))
+    (nick-is-in-channel! channel nick 'unknown)
+    (for-each-plugin
+     (lambda (plugin)
+       ((plugin-join! plugin)
+	nick
+	channel)))
+    (check-ops nick #f)))
+
+(define (part message)
+  (let ((channel (irc:message-receiver message))
+	(nick (irc:message-sender message)))
+    (for-each-plugin
+     (lambda (plugin)
+       ((plugin-leave! plugin)
+	nick
+	channel)))
+    (nick-is-not-in-channel! channel nick)))
+
+(define (nick-mode message)
+  (let* ((channel (car (irc:message-parameters message)))
+	 (modeflags (cadr (irc:message-parameters message)))
+	 (nick (caddr (irc:message-parameters message))))
+    (nick-is-in-channel! channel nick
+			 (cond
+			   ((string=? "+o" modeflags) #t)
+			   ((string=? "-o" modeflags) #f)
+			   (else 'unknown)))))
+
+(define (channel-mode message)
+  (let ((channel (car (irc:message-parameters message)))
+	(modeflags (cadr (irc:message-parameters message))))
+    #f))
+
+(define (mode message)
+  (if (= 3 (length (irc:message-parameters message)))
+      (nick-mode message)
+      (channel-mode message))
+  #f)
+
+(define (message-dispatch message)
+  (let ((body (cadr (irc:message-parameters message))))
+    (cond
+     ((string=? (irc:message-receiver message) *botnick*)
+      (privmsg (irc:message-sender message) body))
+     ((string-match *mention-re* body)
+      (channel-mention  (irc:message-sender message)  (irc:message-receiver message) body))
+     (else
+      (nick-is-in-channel! (irc:message-receiver message) (irc:message-sender message) 'unknown)
+      (for-each-plugin
+       (lambda (plugin)
+	 ((plugin-speak! plugin)
+	  (irc:message-sender message)
+	  (irc:message-receiver message)
+	  body)))))
+    #f))
+
+(define (invite message)
+  (let* ((nick (irc:message-sender message))
+	 (channel (cadr (irc:message-parameters message))))
+    (if (has-power? nick 'invite)
+	(ensure-channel-membership! channel))))
+
+(define (names message)
+  (let* ((channel (caddr (irc:message-parameters message)))
+	 (names (string-split (cadddr (irc:message-parameters message)))))
+    (for-each
+     (lambda (name)
+       (if (eq? #\@ (string-ref name 0))
+	   (nick-is-in-channel! channel (string-drop name 1) #t)
+	   (nick-is-in-channel! channel name #f)))
+     names)))
+
+(define (irc-error message)
+  (printf "IRC ERROR: ~A\n" (irc:message-body message)))
+
+(define (test message)
+  (printf
+   "prefix:~S command:~S timestamp:~S code:~S body:~S parameters:~S index:~S sender:~S receiver:~S\n"
+   (irc:message-prefix message)
+   (irc:message-command message)
+   (irc:message-timestamp message)
+   (irc:message-code message)
+   (irc:message-body message)
+   (irc:message-parameters message)
+   (irc:message-index message)
+   (irc:message-sender message)
+   (irc:message-receiver message))
+
+  #f)
+
+;; Initialisation
+
+(let reconnect ()
+  (irc:connect *con*)
+
+  #;(irc:add-message-handler!
+  *con* test)
+
+  (irc:add-message-handler!
+   *con* irc-error command: "ERROR")
+  
+  (irc:add-message-handler!
+   *con* message-dispatch command: "PRIVMSG")
+  
+  (irc:add-message-handler!
+   *con* join command: "JOIN")
+  
+  (irc:add-message-handler!
+   *con* part command: "PART")
+  
+  (irc:add-message-handler!
+   *con* mode command: "MODE")
+  
+  (irc:add-message-handler!
+   *con* invite command: "INVITE")
+  
+  (irc:add-message-handler!
+   *con* names code: 353)
+  
+  (load-config! (with-input-from-file *conf-filename* read-file))
+  
+  (condition-case (irc:run-message-loop *con* debug: #t pong: #t)
+		  (ex (i/o net)
+		      (irc:disconnect *con*)
+		      (sleep 10)
+		      (irc:connect *con*)
+		      (reconnect))))
